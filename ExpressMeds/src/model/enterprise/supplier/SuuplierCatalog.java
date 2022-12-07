ed8cdc122ed8ea73b229934014e9d645df680711
Neha Joisher <nehajoisher@Nehas-MacBook-Pro.local>
2022-12-07 13:40:22 -0500

    Model Supplier and updated Supplier UI

---
+++ ExpressMeds/src/model/enterprise/supplier/SuuplierCatalog.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.enterprise.supplier;
+
+import java.util.ArrayList;
+import model.enterprise.Enterprise;
+
+/**
+ *
+ * @author nehajoisher
+ */
+public class SuuplierCatalog {
+     ArrayList<Enterprise> supp; 
+}
