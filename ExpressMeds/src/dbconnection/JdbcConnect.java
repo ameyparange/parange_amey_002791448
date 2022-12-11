ff43f2139dec1950c7783e40c89e16f7fa1e64f1
Amey Parange <parange.a@northeastern.edu>
2022-12-11 15:49:41 -0500

    Remove Conflict

--- ExpressMeds/src/dbconnection/JdbcConnect.java
+++ ExpressMeds/src/dbconnection/JdbcConnect.java
@@ -56,9 +56,7 @@ public class JdbcConnect {
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
-            this.con = DriverManager.getConnection("jdbc:mysql://localhost/expressmeddb", "root", "");
-
-            this.con = DriverManager.getConnection("jdbc:mysql://localhost/expressmeddb", "root", "");
+            this.con = DriverManager.getConnection("jdbc:mysql://localhost/expressmeddb", "root", "amey@1105");
 
             this.con.setAutoCommit(false);
         } catch (Exception e) {
